aa5a47df790c07567fbfbd6292e71533d275e168
EAG <rauulssanchezz@gmail.com>
2023-12-12 18:54:45 +0100

    sss

---
+++ app/src/main/java/com/example/examendiseoraul/Ver.kt
@@ -0,0 +1,12 @@
+package com.example.examendiseoraul
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver)
+        intent
+    }
+}
